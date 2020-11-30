28429708db5c584608095f38bd743fbd02077dd7
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2020-11-30 18:02:16 +0100

    cleaned for eMLR part only

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -115,7 +115,7 @@ wflow_publish(here::here(
 message = "use setup child Rmd file")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "XXX")
+wflow_publish(all = TRUE, message = "first rebuild on ETH server")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
@@ -127,13 +127,8 @@ wflow_publish(here::here(
     "index.Rmd",
     "config_dependencies.Rmd",
     "config_nomenclature.Rmd",
-    "config_parameterization.Rmd",
-    "read_World_Ocean_Atlas_2018.Rmd",
-    "read_GLODAPv2_2020.Rmd",
-    "read_GLODAPv2_2016_MappedClimatologies.Rmd",
-    "read_Gruber_2019_Cant.Rmd",
-    "read_Sabine_2004_Cant.Rmd",
-    "read_CO2_atm.Rmd",
+    "config_parameterization_local.Rmd",
+    "eMLR_GLODAPv2_2020_subsetting.Rmd",
     "eMLR_data_preparation.Rmd",
     "eMLR_assumption_testing.Rmd",
     "eMLR_model_fitting.Rmd",
@@ -144,7 +139,7 @@ wflow_publish(here::here(
     "analysis_this_study.Rmd"
   )
 ),
-message = "XXX",
+message = "first rebuild on ETH server",
 republish = TRUE)
 
 
