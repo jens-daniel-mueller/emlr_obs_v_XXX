de11bfe4a76eb2f0c586c4fc18d64826edc3ac5a
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-02-24 16:41:27 +0100

    clean up purrr approach and residual plots

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "purrr residual plots by basin")
+wflow_publish(all = TRUE, message = "clean up purrr approach and residual plots")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
