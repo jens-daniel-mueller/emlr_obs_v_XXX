c45672cf9916024c7d44d9a828351bfd61f44a4b
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-02-24 13:28:46 +0100

    added residual plots

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added plots back to after switching to map aproach")
+wflow_publish(all = TRUE, message = "added residual plots")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
