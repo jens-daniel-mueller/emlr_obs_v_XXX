c3e762b41ef219d4f3ea6f9c48f5c28a5bfc9559
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-03-06 15:27:44 +0100

    Canyon-B comparison for all years together

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "Canyon-B comparison for all years")
+wflow_publish(all = TRUE, message = "Canyon-B comparison for all years together")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
