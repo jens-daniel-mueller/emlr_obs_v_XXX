b697e7a5a1fe5628427136f60739c14d04aa7f02
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-03-23 09:50:53 +0100

    restriction to two eras and new definition procedure

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "included vif removel option")
+wflow_publish(all = TRUE, message = "restriction to two eras and new definition procedure")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
@@ -28,7 +28,7 @@ wflow_publish(here::here(
     "analysis_this_study.Rmd"
   )
 ),
-message = "added vif calculation and plots",
+message = "rerun w/o post 2012 era",
 republish = TRUE)
 
 
