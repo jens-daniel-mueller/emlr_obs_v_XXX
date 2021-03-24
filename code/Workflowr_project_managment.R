b897e7151e0e973ba45672f3102dd080cd81a1df
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-03-24 10:42:35 +0100

    included model data

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "restriction to two eras and new definition procedure")
+wflow_publish(all = TRUE, message = "included model data")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
