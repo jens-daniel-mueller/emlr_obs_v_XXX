2433ed8ee0ddc1afcc718ae61ea1565c797cb2cf
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-03 09:46:28 +0200

    include anomalous changes

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "offset histogram canyon-B for GLODAP prerelease")
+wflow_publish(all = TRUE, message = "include anomalous changes")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
