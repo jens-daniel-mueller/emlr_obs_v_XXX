0e89bd25f289be4f72c1c505abb001555181d08d
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-04 08:08:04 +0200

    clean anomalous changes

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "include anomalous changes")
+wflow_publish(all = TRUE, message = "clean anomalous changes")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
