807c560b2f986d407508fce1201f50ef2e217d11
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-14 15:16:49 +0200

    revised delta cant patterns for mod data

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revised zonal mean section plots for mod data")
+wflow_publish(all = TRUE, message = "revised delta cant patterns for mod data")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
