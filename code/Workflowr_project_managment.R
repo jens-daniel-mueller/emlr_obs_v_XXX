c88292181ca49de47b6b947c92f87cb4dd497b84
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-15 22:21:33 +0200

    vertical bias profile

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "included budget assesment")
+wflow_publish(all = TRUE, message = "vertical bias profile")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
