03a41f4b426b07af802f53890a343419e1753b99
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-15 21:56:22 +0200

    included budget assesment

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revise plots")
+wflow_publish(all = TRUE, message = "included budget assesment")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
