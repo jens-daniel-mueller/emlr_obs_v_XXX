6873303fbf84e4d1504fca4bf691e833a45c2307
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-15 11:11:47 +0200

    alpha model truth analysed

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revised delta cant patterns for mod data")
+wflow_publish(all = TRUE, message = "alpha model truth analysed")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
