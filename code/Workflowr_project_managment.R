cdc534fe7a6fd619e3e720ee6dbf90a2c1552c15
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-16 15:07:55 +0200

    reverted nss sign

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "non steady state changes added")
+wflow_publish(all = TRUE, message = "reverted nss sign")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
