80ae560fb7ea4c6e667c01288356fd1f7c85fdb4
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-29 17:29:11 +0200

    plot spatial residual ditribution

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revised plot obs grid per era")
+wflow_publish(all = TRUE, message = "plot spatial residual ditribution")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
