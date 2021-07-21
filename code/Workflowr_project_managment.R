ee0e941c6aa4a59ce6daa407e6da093cb015145d
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-07-21 14:59:42 +0200

    surface dcant mapping with preformed DIC

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added histogram")
+wflow_publish(all = TRUE, message = "surface dcant mapping with preformed DIC")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
