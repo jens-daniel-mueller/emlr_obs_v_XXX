2477316bf562b71880f94a298c940714320f4e1d
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-07-23 13:41:38 +0200

    rebuild: surface dcant mapping seperate

--- code/local_job_run_case.R
+++
@@ -1,7 +0,0 @@
-rstudioapi::jobRunScript(
-  path = "/UP_home/jenmueller/Projects/emlr_cant/observations/emlr_obs_v_XXX/code/local_job_define_case/v_109.R",
-  encoding = "unknown",
-  workingDir = "/UP_home/jenmueller/Projects/emlr_cant/observations/emlr_obs_v_XXX",
-  importEnv = FALSE,
-  exportEnv = ""
-)
