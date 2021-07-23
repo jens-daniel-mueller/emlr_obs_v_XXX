0812056f453dd7df8dd73bae8bdfa77c0476d6be
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-07-23 10:32:51 +0200

    calculate slab dcant SD

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "surface dcant mapping seperate")
+wflow_publish(all = TRUE, message = "calculate slab dcant SD")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
@@ -32,7 +32,7 @@ wflow_publish(here::here(
     "analysis_anomalous_changes.Rmd"
   )
 ),
-message = "surface dcant mapping with preformed DIC",
+message = "surface dcant mapping seperate",
 republish = TRUE)
 
 
