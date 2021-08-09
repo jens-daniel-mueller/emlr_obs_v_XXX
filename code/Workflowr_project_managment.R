f0efda597ef431924af0136a4a4b5e5ce32b64b0
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-08-09 13:33:34 +0200

    revised canyon-b cleaning and created stats files

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revised canyon-b cleaning")
+wflow_publish(all = TRUE, message = "revised canyon-b cleaning and created stats files")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
