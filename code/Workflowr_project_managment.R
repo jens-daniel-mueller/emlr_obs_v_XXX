73cc28037f66d00f874c3fd465a498f407ce328f
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-09-22 11:37:04 +0200

    new vif plots added

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "surface delta cant with disequilibrium")
+wflow_publish(all = TRUE, message = "new vif plots added")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
