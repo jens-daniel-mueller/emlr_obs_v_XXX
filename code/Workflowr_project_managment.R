67a759e4a12aa7947dd10214cf453db5706344c4
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-10-01 14:28:48 +0200

    code cleaning

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "new vif plots added")
+wflow_publish(all = TRUE, message = "code cleaning")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
@@ -32,7 +32,7 @@ wflow_publish(here::here(
     "analysis_anomalous_changes.Rmd"
   )
 ),
-message = "rerun with 7 predictors",
+message = "test 1990 cutoff for first era",
 republish = TRUE)
 
 
@@ -63,14 +63,23 @@ files <- c(
 
 Version_IDs_1 <- list.files(
   path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
-  pattern = "v_15")#[20:23]
+  pattern = "v_10")[8]
 
 Version_IDs_2 <- list.files(
   path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
-  pattern = "v_25")#[16:23]
+  pattern = "v_26")
+
+Version_IDs_3 <- list.files(
+  path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
+  pattern = "v_30")[8]
 
 Version_IDs <- c(
   Version_IDs_1,
+  Version_IDs_2,
+  Version_IDs_3
+  )
+
+Version_IDs <- c(
   Version_IDs_2
   )
 
