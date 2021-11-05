fc1d8fd82f664b92319eb017b6a172c53e5c731d
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-05 09:02:01 +0100

    plotted C* profiles

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "zonal mean section plots modified")
+wflow_publish(all = TRUE, message = "plotted C* profiles")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
