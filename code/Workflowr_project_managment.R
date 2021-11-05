6ddc78bda1316dc0926f2178ea09616068206ff6
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-05 15:48:10 +0100

    plotted C* sd at depth

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "plotted C* gamma slab maps")
+wflow_publish(all = TRUE, message = "plotted C* sd at depth")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
