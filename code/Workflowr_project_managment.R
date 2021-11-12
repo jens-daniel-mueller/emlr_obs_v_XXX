13a030c8084cbb2364b390de2d68ff199bdc6b54
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-12 11:15:33 +0100

    add zonal mean section of gridded dcant per CFC

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "add zonal mean section of gridded dcant and CFC")
+wflow_publish(all = TRUE, message = "add zonal mean section of gridded dcant per CFC")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
