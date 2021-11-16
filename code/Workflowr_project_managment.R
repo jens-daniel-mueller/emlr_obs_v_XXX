b4734fdc6a4fac2a62fe50c45b2f67febbc5f61b
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-16 15:46:24 +0100

    added plots for C* components

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added plots per gamma level")
+wflow_publish(all = TRUE, message = "added plots for C* components")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
