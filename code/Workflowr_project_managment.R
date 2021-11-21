acf8f2a8c3fcdb20872fa34ac6a8f1a18b4af03f
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-21 20:48:33 +0100

    manual commit

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added correlation plots for C* components")
+wflow_publish(all = TRUE, message = "test C* correction IO")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
@@ -33,7 +33,7 @@ wflow_publish(here::here(
     "tracers_GLODAPv2_2021.Rmd"
   )
 ),
-message = "rerun standard config with tracer analysis",
+message = "test with revised C* correction IO",
 republish = TRUE)
 
 
@@ -69,7 +69,8 @@ files <- c(
   "analysis_zonal_sections.Rmd",
   "analysis_slab_inventory.Rmd",
   "analysis_MLR_performance.Rmd",
-  "analysis_anomalous_changes.Rmd"
+  "analysis_anomalous_changes.Rmd",
+  "tracers_GLODAPv2_2021.Rmd"
 )
 
 Version_IDs_1 <- list.files(
@@ -78,7 +79,7 @@ Version_IDs_1 <- list.files(
 
 Version_IDs_2 <- list.files(
   path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
-  pattern = "v_2c")#[5]
+  pattern = "v_2s")#[5]
 
 Version_IDs_3 <- list.files(
   path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
