f18adbd1c5dd7a9c31223319c6a62de37027fc59
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-11-26 14:13:13 +0100

    added crossover analysis plot

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added map with IO expocodes")
+wflow_publish(all = TRUE, message = "added crossover analysis plot")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
