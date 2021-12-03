459b7d9f5c50c2089c3bdecf89208da2863a2e59
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-12-04 00:31:28 +0100

    corrected filtering, plotted crossover data

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "plot only defined density slabs")
+wflow_publish(all = TRUE, message = "corrected filtering, plotted crossover data")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
@@ -75,11 +75,11 @@ files <- c(
 
 Version_IDs_1 <- list.files(
   path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
-  pattern = "v_10")#[8]
+  pattern = "v_17")#[8]
 
 Version_IDs_2 <- list.files(
   path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
-  pattern = "v_2s")#[5]
+  pattern = "v_27")#[5]
 
 Version_IDs_3 <- list.files(
   path = "/nfs/kryo/work/jenmueller/emlr_cant/observations",
@@ -87,8 +87,7 @@ Version_IDs_3 <- list.files(
 
 Version_IDs <- c(
   Version_IDs_1,
-  Version_IDs_2,
-  Version_IDs_3
+  Version_IDs_2
   )
 
 Version_IDs <- c(
