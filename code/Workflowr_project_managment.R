6beaf430efa48f0d39bb5901e7c7f0e3355292ed
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-16 13:19:13 +0200

    calculated cstar offset era bias

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "calculated cstar offset predicted vs true")
+wflow_publish(all = TRUE, message = "calculated cstar offset era bias")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
