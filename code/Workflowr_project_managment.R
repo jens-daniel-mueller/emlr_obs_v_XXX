5d282dc310dbe7d0dd5898d7fe3aad78f3cc2fd5
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-17 09:35:38 +0200

    included budget calculation

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "included column inventory maps and calculation")
+wflow_publish(all = TRUE, message = "included budget calculation")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
