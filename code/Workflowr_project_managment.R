fd54fecdcd710079feb6eb3a3f0100dae36f1b81
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-22 15:52:35 +0200

    revised budget calcualtion and plots

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "included budget calcualtion and plots")
+wflow_publish(all = TRUE, message = "revised budget calcualtion and plots")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
