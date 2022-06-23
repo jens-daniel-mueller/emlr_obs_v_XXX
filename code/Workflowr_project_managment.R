45ffc5f6ddd0bfc3e932e72f8050f60acc9e57ca
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-23 10:23:03 +0200

    calculated combined cstar contribution

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added C* misfit analysis")
+wflow_publish(all = TRUE, message = "calculated combined cstar contribution")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
