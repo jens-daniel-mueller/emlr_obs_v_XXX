e1f9685b17be5326614d00be844fa3829b004155
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-24 12:05:53 +0200

    slab bias decomposition

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "budget bias decomposition")
+wflow_publish(all = TRUE, message = "slab bias decomposition")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
