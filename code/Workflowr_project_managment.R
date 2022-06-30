c3d49ab5aa6e831eabcb4f39c22933b5514dff50
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-30 12:13:44 +0200

    included dissic changes for all runs

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "included basin hemisphere budgets")
+wflow_publish(all = TRUE, message = "included dissic changes for all runs")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
