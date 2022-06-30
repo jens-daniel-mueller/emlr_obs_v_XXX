5bb586f63243e8e272a0eb28039156facc6d1069
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-30 09:51:13 +0200

    included basin hemisphere budgets

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "included model drift assesment")
+wflow_publish(all = TRUE, message = "included basin hemisphere budgets")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
