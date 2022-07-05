19352ef06bfc7b3d3c4f673e1dc4d4471bd434e0
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-07-05 23:38:30 +0200

    use cstar B drift

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "use cstar A files")
+wflow_publish(all = TRUE, message = "use cstar B drift")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
